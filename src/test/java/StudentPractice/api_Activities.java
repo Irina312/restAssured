71e77905173875ab59c25e76a18e072534f83c56
Irina312 <irina.evans@yahoo.com>
2022-07-01 22:16:08 -0400

    New tests added

---
+++ src/test/java/StudentPractice/api_Activities.java
@@ -0,0 +1,65 @@
+package StudentPractice;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.testng.annotations.Test;
+
+public class api_Activities {
+    // TODO:
+    // Testing URI = https://fakerestapi.azurewebsites.net/api/v1/
+
+    // Task 1: Using Rest Assured validate the status code for endpoint /Activities
+
+    // Task 2: Using Rest Assured Validate Content-Type  is application/json; charset=utf-8; v=1.0
+    // for endpoint /Activities
+
+    // Task 3: Using Rest Assured validate the status code for endpoint /Activities/12
+
+    // Task 4: Using Rest Assured Validate Content-Type  is application/json; charset=utf-8; v=1.0
+    // for endpoint /Activities/12
+
+    @Test
+    public void verifyStatusCode() {
+        RestAssured.given()// allows to build the request
+                .when()
+                .get("https://fakerestapi.azurewebsites.net/api/v1/Activities")
+                .then() // allows to do validation
+                .assertThat()
+                .statusCode(200);
+    }
+    @Test
+    public void verifyContentType() {
+        RestAssured.given()
+                .when()
+                .get("https://fakerestapi.azurewebsites.net/api/v1/Activities/")
+                .then()
+                .assertThat()
+                .contentType(ContentType.JSON);
+    }
+    @Test
+    public void verifyContentTypeOption2() {
+        RestAssured.given()
+                .when()
+                .get("https://fakerestapi.azurewebsites.net/api/v1/Activities/")
+                .then()
+                .assertThat().header("Content-Type", "application/json; charset=utf-8; v=1.0");
+    }
+    @Test
+    public void verifyStatusCode12() {
+        RestAssured.given()
+                .when()
+                .get("https://fakerestapi.azurewebsites.net/api/v1/Activities/12")
+                .then()
+                .assertThat()
+                .statusCode(200);
+    }
+    @Test
+    public void verifyContentType12() {
+        RestAssured.given()
+                .when()
+                .get("https://fakerestapi.azurewebsites.net/api/v1/Activities/12")
+                .then()
+                .assertThat()
+                .contentType(ContentType.JSON);
+    }
+}
